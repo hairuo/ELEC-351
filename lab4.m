b33154d988825ad8f1b06230f9548b126e7adade
hairuo <hairuo@gmail.com>
2025-12-01 17:29:26 -0500

    lab4

--- lab4.m
+++ lab4.m
@@ -1,7 +1,7 @@
 %2025-11-21
 %% 
 clear, clc, clf, close all
-Zmm = 123 - j*30; %measured match load 50 Ohm
-Gamma =(Zmm - 50)/(Zmm + 50)
+Zmm = [98.8-j*40.4; 30+j*10.3] %measured match load 50 Ohm
+Gamma =(Zmm - 50)./(Zmm + 50)
 AbsG = abs(Gamma)
 S11 = 20*log10(AbsG)
